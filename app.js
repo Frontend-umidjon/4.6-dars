c3cdf9b8c5afc118608f4989b1974d7c87fea570
Frontend-Umidjon <umidjonkengashev6@gamil.com>
2024-11-21 22:42:59 +0500

    uy ishi

---
+++ app.js
@@ -0,0 +1,68 @@
+// CODEWARS
+
+// 1-masala
+// Array ichidagi eng kichkina sonni chiqaradigan funksiya
+{
+    function findSmallestInt(arr) {
+    return Math.min(...arr); ;
+}
+
+}
+// 2-masala
+ // Stringni numberga o'zgartiradigan funksiya
+{
+    const stringToNumber = function(str){
+    return Number(str);
+}
+}
+
+// 3-masala
+// Songacha bo'lgan barcha sonlarni qo'shadigan funksiya
+{
+    var summation = function (num) {
+    return num * (num + 1) / 2;
+}
+}
+
+// 4-masala
+// Oddiy hello world funksiyasi
+{
+   function greet (){
+  return "hello world!"
+} 
+}
+
+// 5-masala
+// Array ichidagi boolean qiymatlar  sonini chiqaradigan funksiya
+{
+    function countSheeps(sheep) {
+    return sheep.filter(Boolean).length;
+  
+}
+}
+
+// 6-masala
+// String ni bo'shlig'ini o'chiradigan funksiya
+{
+    function noSpace(x){
+    return x.split(" ").join("");
+
+}
+}
+
+// 7-masala
+// Integer ni 2 ga ko'paytiradigan  funksiya
+{
+    function doubleInteger(i) {
+  return i*2;
+}
+}
+
+// 8-masala
+// kiritilgan ism bilan salomlashadigan funksiya
+{
+    function greet(name){
+    return "Hello, " + name + " how are you doing today?";
+  
+}
+}
